4c192d6a23a08b149d78620c971ec2b8a2b8779e
M4deN <alexdesaran@outlook.com>
2023-10-11 11:25:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,116 +1,107 @@
 document.addEventListener('DOMContentLoaded', () => {
     const gameCanvas = document.getElementById('gameCanvas');
     const ctx = gameCanvas.getContext('2d');
-    const startButton = document.getElementById('startGame');
+    let snake = [{ x: 10, y: 10 }];
+    let food = { x: 15, y: 15 };
+    let dx = 1;
+    let dy = 0;
+    let score = 0;
+    let gameInterval;
 
-    toggleButton.addEventListener('click', () => {
-        document.documentElement.classList.toggle('dark-mode');
-    });
+    function drawSnake() {
+        ctx.fillStyle = '#00f';
+        snake.forEach(segment => {
+            ctx.fillRect(segment.x * 20, segment.y * 20, 20, 20);
+        });
+    }
 
-    startButton.addEventListener('click', () => {
-        startGame();
-    });
+    function drawFood() {
+        ctx.fillStyle = '#f00';
+        ctx.fillRect(food.x * 20, food.y * 20, 20, 20);
+    }
 
-    function startGame() {
-        let snake = [{ x: 10, y: 10 }];
-        let food = { x: 15, y: 15 };
-        let dx = 1;
-        let dy = 0;
-        let score = 0;
-        let lastTimestamp = 0;
-        let interval = 200;
+    function updateSnake(timestamp) {
+        const head = { x: snake[0].x + dx, y: snake[0].y + dy };
 
-        function drawSnake() {
-            ctx.fillStyle = '#00f';
-            snake.forEach(segment => {
-                ctx.fillRect(segment.x * 20, segment.y * 20, 20, 20);
-            });
+        if (head.x < 0) head.x = 19;
+        if (head.x >= 20) head.x = 0;
+        if (head.y < 0) head.y = 19;
+        if (head.y >= 20) head.y = 0;
+
+        snake.unshift(head);
+
+        if (head.x === food.x && head.y === food.y) {
+            score++;
+            food = { x: Math.floor(Math.random() * 20), y: Math.floor(Math.random() * 20) };
+        } else {
+            snake.pop();
         }
 
-        function drawFood() {
-            ctx.fillStyle = '#f00';
-            ctx.fillRect(food.x * 20, food.y * 20, 20, 20);
+        ctx.clearRect(0, 0, gameCanvas.width, gameCanvas.height);
+
+        drawSnake();
+        drawFood();
+
+        if (checkCollision()) {
+            alert(`Game Over! Pontuação: ${score}`);
+            clearInterval(gameInterval);
+            gameInterval = null;
+            snake = [{ x: 10, y: 10 }];
+            dx = 1;
+            dy = 0;
+            score = 0;
         }
+    }
 
-        function updateSnake(timestamp) {
-            if (timestamp - lastTimestamp >= interval) {
-                lastTimestamp = timestamp;
+    function checkCollision() {
+        const head = snake[0];
+        return (
+            snake.slice(1).some(segment => segment.x === head.x && segment.y === head.y)
+        );
+    }
 
-                const head = { x: snake[0].x + dx, y: snake[0].y + dy };
-
-                if (head.x < 0) head.x = 19;
-                if (head.x >= 20) head.x = 0;
-                if (head.y < 0) head.y = 19;
-                if (head.y >= 20) head.y = 0;
-
-                snake.unshift(head);
-
-                if (head.x === food.x && head.y === food.y) {
-                    score++;
-                    food = { x: Math.floor(Math.random() * 20), y: Math.floor(Math.random() * 20) };
-                } else {
-                    snake.pop();
+    function handleKeydown(event) {
+        switch (event.key) {
+            case 'ArrowUp':
+                if (dy !== 1) {
+                    dx = 0;
+                    dy = -1;
                 }
-
-                ctx.clearRect(0, 0, gameCanvas.width, gameCanvas.height);
-
-                drawSnake();
-                drawFood();
-
-                if (checkCollision()) {
-                    alert(`Game Over! Pontuação: ${score}`);
-                    snake = [{ x: 10, y: 10 }];
+                break;
+            case 'ArrowDown':
+                if (dy !== -1) {
+                    dx = 0;
+                    dy = 1;
+                }
+                break;
+            case 'ArrowLeft':
+                if (dx !== 1) {
+                    dx = -1;
+                    dy = 0;
+                }
+                break;
+            case 'ArrowRight':
+                if (dx !== -1) {
                     dx = 1;
                     dy = 0;
-                    score = 0;
-                    interval = 200;
                 }
-            }
-
-            requestAnimationFrame(updateSnake);
+                break;
         }
-
-        function checkCollision() {
-            const head = snake[0];
-            return (
-                snake.slice(1).some(segment => segment.x === head.x && segment.y === head.y)
-            );
-        }
-
-        function handleKeydown(event) {
-            switch (event.key) {
-                case 'ArrowUp':
-                    if (dy !== 1) {
-                        dx = 0;
-                        dy = -1;
-                    }
-                    break;
-                case 'ArrowDown':
-                    if (dy !== -1) {
-                        dx = 0;
-                        dy = 1;
-                    }
-                    break;
-                case 'ArrowLeft':
-                    if (dx !== 1) {
-                        dx = -1;
-                        dy = 0;
-                    }
-                    break;
-                case 'ArrowRight':
-                    if (dx !== -1) {
-                        dx = 1;
-                        dy = 0;
-                    }
-                    break;
-            }
-        }
-
-        document.addEventListener('keydown', handleKeydown);
-
-        updateSnake(0);
     }
+
+    document.addEventListener('keydown', handleKeydown);
+
+    // Adicione um event listener para o botão iniciar jogo
+    const startButton = document.getElementById('startGame');
+    startButton.addEventListener('click', () => {
+        if (!gameInterval) {
+            gameInterval = setInterval(updateSnake, 200);
+        }
+    });
+    
 });
+
+
 
 function enableDarkMode() {
     document.body.classList.add('dark-mode');
@@ -129,10 +120,10 @@ function enableDarkMode() {
   
     if (storedDarkMode === 'enabled') {
       enableDarkMode();
-      document.getElementById('dark-mode-toggle').classList.add('fa-moon');
+      document.getElementById('dark-mode-toggle').classList.add('fa-sun');
     } else {
       disableDarkMode();
-      document.getElementById('dark-mode-toggle').classList.add('fa-sun');
+      document.getElementById('dark-mode-toggle').classList.add('fa-moon');
     }
   });
   
@@ -143,11 +134,11 @@ function enableDarkMode() {
     const darkModeEnabled = body.classList.contains('dark-mode');
   
     if (!darkModeEnabled) {
-      this.classList.remove('fa-sun');
-      this.classList.add('fa-moon');
-    } else {
       this.classList.remove('fa-moon');
       this.classList.add('fa-sun');
+    } else {
+      this.classList.remove('fa-sun');
+      this.classList.add('fa-moon');
     }
   
     // Após mudar a classe, verifique novamente o modo escuro
